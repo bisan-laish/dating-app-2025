40752f10fa9c07dadd6916f8fc7fcfe49ade2486
bisan8 <bisan.laish@gmail.com>
2025-11-15 22:50:26 -0500

    End of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,40 +1,14 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, Inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = signal('Dating app');
-  protected members = signal<User[]>([]);
-
-  ngOnInit(): void {
-    this.getMembers();
-    this.setCurrentUser();
-  }
-
-  private getMembers() {
-    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
-      next: response => this.members.set(response),
-      error: error => console.log(error),
-      complete: () => console.log('The http request is completed')
-    });
-  }
-
-  setCurrentUser(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
+export class App {
+  protected router = inject(Router);
 
 }
